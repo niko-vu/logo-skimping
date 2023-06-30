17f803bdb2e7b0a0764cbbc6d26a62decb3d1b7f
fearoffun <480dunce@gmail.com>
2023-06-30 13:40:34 -0700

    separated the svg generator file into three different ones for their respective shapes and updated the index.js file to reflect that. also altered the writefile functions so that they would write files to the examples folder to keep things organized.

--- README.md
+++ README.md
@@ -33,6 +33,8 @@ The deployed application should look something like this:
 * I referenced this [Professional README Guide tutorial](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide) for the structure of my README and the generateMarkdown.js file I modified.
 * [1](https://www.golinuxcloud.com/svg-javascript/#:~:text=One%20way%20to%20create%20an,that%20make%20up%20the%20image.) + [2](https://javascript.plainenglish.io/how-to-create-an-svg-element-with-vanilla-javascript-a6b140745196) + [3](https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts#step-4-using-lists-raw-lists-expandable-lists-checkboxes-passwords-and-editors)
 * This requires [npm inquirer 8.2.4](https://www.npmjs.com/package/inquirer/v/8.2.4) and [npm jest](https://www.npmjs.com/package/jest?activeTab=code).
+* Referenced this [PBPhpsolutions article](https://pbphpsolutions.com/write-a-file-into-specific-folder-in-node-js-example.html?expand_article=1) on writing a file into a specific folder in Node JS.
+* Referenced this [W3Schools page](https://www.w3schools.com/js/js_if_else.asp) on conditional statements to.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,54 +1,72 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateSVG = require('./lib/generateSVG');
+const path = require('path');
+const directory = path.join(__dirname, 'examples/');
+const generateCircle = require('./lib/generateCircle');
+const generateSquare = require('./lib/generateSquare');
+const generateTriangle = require('./lib/generateTriangle');
 
+function writeToFileCircle(data) {
+    const filename = `${data.characters}.svg`;
 
-// const svg = document.createElementNS('<http://www.w3.org/2000/svg>', 'svg')
-// svg.setAttribute('width', '300');
-// svg.setAttribute('height', '200');
+    fs.writeFile(directory+filename, generateCircle(data), (err) => err ? console.log(err) : console.log('Success!')
+    );
+}
 
+function writeToFileSquare(data) {
+    const filename = `${data.characters}.svg`;
 
-function writeToFile(data) {
-    const logo = 'logo';
-    const filename = `${logo}.svg`;
+    fs.writeFile(directory+filename, generateSquare(data), (err) => err ? console.log(err) : console.log('Success!')
+    );
+}
 
-    fs.writeFile(filename, generateSVG(data), (err) => err ? console.log(err) : console.log('Success!')
+function writeToFileTriangle(data) {
+    const filename = `${data.characters}.svg`;
+
+    fs.writeFile(directory+filename, generateTriangle(data), (err) => err ? console.log(err) : console.log('Success!')
     );
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'characters',
-            message: 'Choose 3 characters for the logo.',
-        },
-        {
-            type: 'input',
-            name: 'txtcolor',
-            message: 'Choose a color for the characters.',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape for your logo.',
-            choices: ['circle', 'triangle', 'square'],
-        },
-        {
-            type: 'input',
-            name: 'shapecolor',
-            message: 'Choose a color for your shape.',
-        },
-    ])
+        .prompt([
+            {
+                type: 'input',
+                name: 'characters',
+                message: 'Choose 3 characters for the logo.',
+            },
+            {
+                type: 'input',
+                name: 'charcolor',
+                message: 'Choose a color for the characters.',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose a shape for your logo.',
+                choices: ['circle', 'square', 'triangle'],
+            },
+            {
+                type: 'input',
+                name: 'shapecolor',
+                message: 'Choose a color for your shape (enter a color keyword or hexadecimal number).',
+            },
+        ])
 
-    .then((data) => {
-        const filename = `${data.characters.toLowerCase().split(' ').join('')}.json`;
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) => err ? console.log(err) : console.log('Success!')
-        );
-        writeToFile(data);
-    });
+        .then((data) => {
+            const filename = `${data.characters.toLowerCase().split(' ').join('')}.json`;
+            fs.writeFile(directory+filename, JSON.stringify(data, null, '\t'), (err) => err ? console.log(err) : console.log('Success!')
+            );
+
+            if (data.shape == "circle") {
+                writeToFileCircle(data);
+            } else if (data.shape == "square") {
+                writeToFileSquare(data);
+            } else {
+                writeToFileTriangle(data);
+            };
+        });
 }
 
 // Function call to initialize app
